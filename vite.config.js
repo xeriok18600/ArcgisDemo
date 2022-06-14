c829e558720add81d4559bf39dfa9ff42fed2505
Kyle Lee <xeriok18600@gmail.com>
2022-06-14 16:29:37 +0800

    [fix] 修改 multi page config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
+const { resolve } = require('path')
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: '0.0.0.0'
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        map: resolve(__dirname, 'map.html')
+      }
+    }
   }
 });
